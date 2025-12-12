fa94d1c3eeac70c10e2cf1b15fb9c6e0234efa8c
kohhorov09 <your-email@example.com>
2025-12-12 16:08:31 +0500

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// Supports only weight 400
-import "@fontsource-variable/roboto-flex";
 createRoot(document.getElementById("root")).render(<App />);
